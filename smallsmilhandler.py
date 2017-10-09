394cf29da3f5d8a097bf157480de9c89fb1717b8
Monica Gomez Avila <mgavila@gamma03.aulas.gsyc.urjc.es>
2017-10-09 15:58:14 +0200

    Eliminadas variables innecesarias y pasado por PEP8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,28 +9,6 @@ class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
 
-        self.width = ""
-        self.height = ""
-        self.bgcolor = ""
-
-        self.id = ""
-        self.top = ""
-        self.bottom = ""
-        self.left = ""
-        self.right = ""
-
-        self.src = ""
-        self.begin = ""
-        self.dur = ""
-        self.region = ""
-
-        self.audiosrc = ""
-        self.audiobegin = ""
-        self.audiodur = ""
-
-        self.txtsrc = ""
-        self.txtregion = ""
-
         self.Letiquetas = []
 
     def startElement(self, name, attrs):
@@ -38,56 +16,39 @@ class SmallSMILHandler(ContentHandler):
         if name == 'root-layout':
             diccionario = {}
             diccionario['etiqueta'] = name
-            self.width = attrs.get('width', "")
-            diccionario['width'] = self.width
-            self.height = attrs.get('height', "")
-            diccionario['heigth'] = self.height
-            self.bgcolor = attrs.get('background-color', "")
-            diccionario['bgcolor'] = self.bgcolor
+            diccionario['width'] = attrs.get('width', "")
+            diccionario['heigth'] = attrs.get('height', "")
+            diccionario['bgcolor'] = attrs.get('background-color', "")
             self.Letiquetas.append(diccionario)
         elif name == 'region':
             diccionario = {}
             diccionario['etiqueta'] = name
-            self.id = attrs.get('id', "")
-            diccionario['id'] = self.id
-            self.top = attrs.get('top', "")
-            diccionario['top'] = self.top
-            self.bottom = attrs.get('bottom', "")
-            diccionario['bottom'] = self.bottom
-            self.left = attrs.get('left', "")
-            diccionario['left'] = self.left
-            self.right = attrs.get('right', "")
-            diccionario['right'] = self.right
+            diccionario['id'] = attrs.get('id', "")
+            diccionario['top'] = attrs.get('top', "")
+            diccionario['bottom'] = attrs.get('bottom', "")
+            diccionario['left'] = attrs.get('left', "")
+            diccionario['right'] = attrs.get('right', "")
             self.Letiquetas.append(diccionario)
         elif name == 'img':
             diccionario = {}
             diccionario['etiqueta'] = name
-            self.src = attrs.get('src', "")
-            diccionario['src'] = self.src
-            self.begin = attrs.get('begin', "")
-            diccionario['begin'] = self.begin
-            self.dur = attrs.get('dur', "")
-            diccionario['dur'] = self.dur
-            self.region = attrs.get('region', "")
-            diccionario['region'] = self.region
+            diccionario['src'] = attrs.get('src', "")
+            diccionario['begin'] = attrs.get('begin', "")
+            diccionario['dur'] = attrs.get('dur', "")
+            diccionario['region'] = attrs.get('region', "")
             self.Letiquetas.append(diccionario)
         elif name == 'audio':
             diccionario = {}
             diccionario['etiqueta'] = name
-            self.audiosrc = attrs.get('src', "")
-            diccionario['src'] = self.audiosrc
-            self.audiobegin = attrs.get('begin', "")
-            diccionario['begin'] = self.audiobegin
-            self.audiodur = attrs.get('dur', "")
-            diccionario['dur'] = self.audiodur
+            diccionario['src'] = attrs.get('src', "")
+            diccionario['begin'] = attrs.get('begin', "")
+            diccionario['dur'] = attrs.get('dur', "")
             self.Letiquetas.append(diccionario)
         elif name == 'textstream':
             diccionario = {}
             diccionario['etiqueta'] = name
-            self.txtsrc = attrs.get('src', "")
-            diccionario['src'] = self.txtsrc
-            self.txtregion = attrs.get('begin', "")
-            diccionario['region'] = self.txtregion
+            diccionario['src'] = attrs.get('src', "")
+            diccionario['region'] = attrs.get('begin', "")
             self.Letiquetas.append(diccionario)
 
     def get_tags(self):
